fa0e388dea80ef8e5e3e5e93720852a5e9d6ad4b
Evgeny Panchenko <worldbatle@mail.ru>
2025-03-02 13:03:04 +0300

    Добавлен ввод переменной

--- HW-1/main.cpp
+++ HW-1/main.cpp
@@ -2,6 +2,13 @@
 
 int main()
 {
-	std::cout << "Hello, World!" << std::endl;
+	double x; // Объявляем переменную x
+
+	std::cout << "Введите значение x: ";
+	std::cin >> x; // Считываем x
+
+	// Проверка: выводим то, что ввёл пользователь
+	std::cout << "Вы ввели x = " << x << std::endl;
+
 	return 0;
 }
